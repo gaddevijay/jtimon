48a576c4b549b2b81220915c14e1d16529f77edb
gaddevijay <vijaygadde@juniper.net>
2018-10-19 09:46:05 +0530

    Update influx.go

--- influx.go
+++ influx.go
@@ -76,6 +76,7 @@ func dbBatchWrite(jctx *JCtx) {
 				}
 
 				jLog(jctx, fmt.Sprintf("Batch processing: #packets:%d #points:%d\n", n, len(bp.Points())))
+				jLog(jctx, bp.Points())
 
 				if err := (*jctx.influxCtx.influxClient).Write(bp); err != nil {
 					jLog(jctx, fmt.Sprintf("Batch DB write failed: %v", err))
